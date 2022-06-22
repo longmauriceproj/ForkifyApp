ba30fc85ffc5563a51874df0359eab08b712c559
Maurice Long <longmaurice255@gmail.com>
2022-06-22 07:16:23 -0700

    added functionality to listen for load and hashchange events to show the recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -28,11 +28,17 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    //get hash from url
+    const id = window.location.hash.slice(1);
+
+    //Guard clause
+    if (!id) return;
+
     //Loading recipe
     renderSpinner(recipeContainer);
 
     const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
@@ -48,7 +54,6 @@ const showRecipe = async function () {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
-    console.log(recipe);
 
     //Rendering recipe
     const html = `
@@ -156,4 +161,6 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
