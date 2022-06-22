2230e727e3afe618a058704b716a9ca6ec67a8d6
Maurice Long <longmaurice255@gmail.com>
2022-06-22 16:39:07 -0700

    implemented functionality to update servings and corresponding ingredient quantities

--- src/js/model.js
+++ src/js/model.js
@@ -49,10 +49,18 @@ export const loadSearchResults = async function (query) {
   }
 };
 
-//function take in page we want to render and only render results we wan to for that page
 export const getSearchResultsPage = function (page = state.search.page) {
   state.search.page = page;
   const start = (page - 1) * state.search.resultsPerPage;
   const end = page * state.search.resultsPerPage;
   return state.search.results.slice(start, end);
+};
+
+export const updateServings = function (newServings) {
+  state.recipe.ingredients.forEach(ing => {
+    ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
+    //newQt = oldQt * newServings / oldServings
+  });
+
+  state.recipe.servings = newServings;
 };
